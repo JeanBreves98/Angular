e834b8a9dfc89a3e5e48722f6f7d45e4566b8b74
Jean <jeanbreves1998@gmail.com>
2025-08-13 15:20:52 -0300

    13/08/2025 - 15:20

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,6 +1,16 @@
-import { Component, Input, computed, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users'
+// type User = {
+//     id: string;
+//     avatar: string;
+//     name: string;
+//   }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -10,13 +20,15 @@ import { DUMMY_USERS } from '../dummy-users'
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
