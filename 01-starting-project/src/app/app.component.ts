91e06976f45a20da2358e284dc07f89e6636ce39
Jean <jeanbreves1998@gmail.com>
2025-08-15 15:30:51 -0300

    15/08/2025 - 15:30

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component'
-
 import { UserComponent } from './user/user.component'
-
 import { DUMMY_USERS } from './dummy-users';
-
 import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
@@ -17,7 +14,7 @@ import { TasksComponent } from './tasks/tasks.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
