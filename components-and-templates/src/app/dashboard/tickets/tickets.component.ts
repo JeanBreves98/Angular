1fc4a789ce1cc07f48c9b5ce5fb8905a7b2c1e3d
Jean <jeanbreves1998@gmail.com>
2025-08-29 18:59:18 -0300

    29/08/2025 - 18:59

--- components-and-templates/src/app/dashboard/tickets/tickets.component.ts
+++ components-and-templates/src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../header/header.component";
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
