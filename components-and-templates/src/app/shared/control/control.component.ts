f67ad5a2345b4715c2a09b3a1761ab4dd98e6e3e
Jean <jeanbreves1998@gmail.com>
2025-09-09 16:33:32 -0300

    09/09/2025 - 16:33

--- components-and-templates/src/app/shared/control/control.component.ts
+++ components-and-templates/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
+import { AfterContentInit, afterNextRender, afterRender, AfterViewInit, Component, contentChild, ContentChild, ContentChildren, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -12,16 +12,33 @@ import { Component, ElementRef, HostBinding, HostListener, inject, input, ViewEn
     '(click)': 'onClick()'
   }
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit{
   // @HostBinding('class') className = 'control'
   // @HostListener('click') onClick() {
   //   console.log('Clicked!');
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>
+  private control = contentChild<ElementRef<HTMLTextAreaElement | HTMLInputElement>>('input');
+
+  constructor() {
+    afterRender(() => {
+      console.log('afterRender');
+    });
+
+    afterNextRender(() => {
+      console.log('afterNextRender');
+    });
+  }
+
+  ngAfterContentInit(): void {
+    // ...
+  }
 
   onClick() {
     console.log('Clicked!');
     console.log(this.el);
+    console.log(this.control);
   }
 }
