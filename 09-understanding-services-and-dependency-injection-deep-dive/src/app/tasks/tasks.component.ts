35eb791beea2b88ce0bea21aeb339dab25b7b8ab
Jean <jeanbreves1998@gmail.com>
2025-10-07 14:57:13 -0300

    07/10/2025 - 14:57

--- 09-understanding-services-and-dependency-injection-deep-dive/src/app/tasks/tasks.component.ts
+++ 09-understanding-services-and-dependency-injection-deep-dive/src/app/tasks/tasks.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskItemComponent } from './tasks-list/task-item/task-item.component';
+// import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  // providers: [TasksService]
 })
 export class TasksComponent {}
--- 09-understanding-services-and-dependency-injection-deep-dive/src/main.ts
+++ 09-understanding-services-and-dependency-injection-deep-dive/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TasksService } from './app/tasks/tasks.service';
+import { LoggingService } from './app/logging.service';
 
+// bootstrapApplication(AppComponent, { providers: [TasksService]}).catch((err) => console.error(err));
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
