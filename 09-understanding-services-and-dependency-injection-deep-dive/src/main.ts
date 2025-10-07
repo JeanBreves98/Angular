0d4db10d906f2fb14268afce2548d0aecb38af51
Jean <jeanbreves1998@gmail.com>
2025-10-07 15:55:17 -0300

    07/10/2025 - 15:55

--- 09-understanding-services-and-dependency-injection-deep-dive/src/main.ts
+++ 09-understanding-services-and-dependency-injection-deep-dive/src/main.ts
@@ -3,6 +3,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { TasksService } from './app/tasks/tasks.service';
 import { LoggingService } from './app/logging.service';
+import { InjectionToken } from '@angular/core';
+
+export const TasksServiceToken = new InjectionToken<TasksService>('tasks-service-token')
 
 // bootstrapApplication(AppComponent, { providers: [TasksService]}).catch((err) => console.error(err));
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { providers: [{ provide: TasksServiceToken, useClass: TasksService}],}).catch((err) => console.error(err));
