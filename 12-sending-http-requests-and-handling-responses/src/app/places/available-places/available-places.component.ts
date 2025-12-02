78dd74b388e9ac8d2cb137f40d38c2b26fb4b880
Jean <jeanbreves1998@gmail.com>
2025-12-02 17:42:43 -0300

    02/12/2025 - 17:42

--- 12-sending-http-requests-and-handling-responses/src/app/places/available-places/available-places.component.ts
+++ 12-sending-http-requests-and-handling-responses/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map, Subscription } from 'rxjs';
+import { catchError, map, Subscription, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,6 +16,7 @@ import { map, Subscription } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef)
 
@@ -26,7 +27,16 @@ export class AvailablePlacesComponent implements OnInit{
     const subscription = this.httpClient
     .get<{places: Place[]}>('http://localhost:3000/places')
     .pipe(
-      map((resData) => resData.places)
+      map((resData) => resData.places), 
+      catchError((error) => {
+        console.log(error);
+        return throwError(
+          () =>
+             new Error(
+              'Something went wrong fetching the available places. Please try again later.'
+            )
+          );
+        })
     )
     // const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places', {
     //   observe: 'response'
@@ -37,6 +47,10 @@ export class AvailablePlacesComponent implements OnInit{
         // console.log(resData.places);
         // console.log(response.body?.places);
       },
+      error: (error: Error) => {
+        console.log(error);
+        this.error.set(error.message);
+      },
       complete: () => {
         this.isFetching.set(false);
       }
@@ -45,6 +59,13 @@ export class AvailablePlacesComponent implements OnInit{
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     })
+  }
 
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient.put('http://localhost:3000/user-places', {
+      placeId: selectedPlace.id
+      }).subscribe({
+        next: (resData) => console.log(resData),
+      });
   }
 }
