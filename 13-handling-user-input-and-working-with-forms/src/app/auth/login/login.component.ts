2e071f5bc032342b12025bdb7c66b9844c01f8fa
Jean <JeanBreves1998@gmail.com>
2026-02-11 16:31:35 -0300

    11/02/2026 - 16:31

--- 13-handling-user-input-and-working-with-forms/src/app/auth/login/login.component.ts
+++ 13-handling-user-input-and-working-with-forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainSpecialCharacter(control: AbstractControl) {
   const regex = /[!@#$%^&*(),.?":{}|<>_\-\\[\]/+=;']/;
@@ -11,6 +12,22 @@ function mustContainSpecialCharacter(control: AbstractControl) {
   return { doesNotContainSpecialCharacter: true };
 }
 
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null);
+  }
+
+  return of({notUnique: true});
+}
+
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem('saved-login-form')
+
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -18,10 +35,32 @@ function mustContainSpecialCharacter(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    // const savedForm = window.localStorage.getItem('saved-login-form');
+
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+    //   });
+    // }
+
+      const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+        next: value => {
+          window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
+        }
+      });
+
+      this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
   form = new FormGroup({
     email: new FormControl('', {
       validators: [ Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
     }),
     password: new FormControl('', {
       validators: [Validators.required, Validators.minLength(6), mustContainSpecialCharacter],
